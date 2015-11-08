31914b0b42b2275e5b1b2064581003ffa9dda40c
Daniel Hurowitz <hurowits@gmail.com>
2015-11-08 16:47:27 +0200

    polishing figures for pub

--- MATLAB/g_ring.m
+++ MATLAB/g_ring.m
@@ -9,7 +9,7 @@ s=20;
 S=s*L;
 
 q = sqrt(epsilon/D-s^2/4)*L;
-epsilon_1 = S^2/4 * G/(G-1)
+epsilon_1 =0; S^2/4 * G/(G-1)
 % epsilon_1=0.0635;1.5*1e-5;0.25
 psi =( 2*(cos(q)+ 1/G * (q.^2+S^2/4)./(2*q) .*sin(q))-2);
 V = log(2*(sqrt(1+(1/G * (q.^2+S^2/4)./(4*q)).^2)-2));
@@ -18,8 +18,10 @@ V = log(2*(sqrt(1+(1/G * (q.^2+S^2/4)./(4*q)).^2)-2));
 %find epsilon_k from continuuous ring and calculate electrostatic potential
 ind = find(abs(diff(sign(psi)))==2); 
 ind2 = ind(1:10);
-[kappa,signKappa,expKappa,E_vec] = thoulessKappa([ epsilon(ind)],N,g,[ epsilon]);
-[kappa2,signKappa2,expKappa2,E_vec2] = thoulessKappa([ epsilon(ind2)],N,g,[ epsilon]);
+% [kappa,signKappa,expKappa,E_vec] = thoulessKappa([epsilon_1 epsilon(ind)],N,g,[ epsilon]);
+% [kappa2,signKappa2,expKappa2,E_vec2] = thoulessKappa([epsilon_1 epsilon(ind2)],N,g,[ epsilon]);
+[kappa,signKappa,expKappa,E_vec] = thoulessKappa([0 epsilon(ind)],N,g,[ epsilon]);
+[kappa2,signKappa2,expKappa2,E_vec2] = thoulessKappa([0 epsilon(ind2)],N,g,[ epsilon]);
 
 figure;
 axes('FontSize',24)
@@ -42,11 +44,11 @@ grid on
 % set(gca,'XLim', ax.XLim);
 % set(gca,'YLim', ax.YLim);
 hold off
-xlabel('\epsilon');
-ylabel('V(\epsilon)')
+xlabel('$\epsilon$','Interpreter','Latex');
+ylabel('$V(\epsilon)$','Interpreter','Latex')
 legend([h3,h2,h1],{'N = 10 ';['N = ',num2str(length(ind))];'N = \infty'},'Location','NorthWest');
-axis([-0 2000 -10 40])
-% print(gcf, '-depsc2', ['/Users/danielhurowitz/PROJ/NEG/Figs/gRing_ES_vs_cont2.eps'])
+axis([-20 2000 -10 40])
+print(gcf, '-depsc2', ['/Users/danielhurowitz/PROJ/NEG/Figs/gRing_ES_vs_cont.eps'])
 %%
 SpectDet=2*(cos(q) + 1/g * (q.^2+S^2/4)./(2*q) .*sin(q))-2;
 figure;
